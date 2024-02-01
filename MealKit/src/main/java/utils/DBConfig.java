9a268eb373a3adedcbfa7d9adcb1f7d31ea0847d
suha <jsh0210hey@gmail.com>
2024-02-01 16:46:58 +0900

    db 접속 주소 수정

--- MealKit/src/main/java/utils/DBConfig.java
+++ MealKit/src/main/java/utils/DBConfig.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConfig {
-<<<<<<< HEAD
+
 	// Oracle 데이터베이스 연결 정보 설정
 	// "jdbc:oracle:thin:@ip주소:포트번호:orcl"
 	private static final String JDBC_URL = "jdbc:oracle:thin:@1.220.247.78:1522:orcl";
@@ -17,15 +17,3 @@ public class DBConfig {
 	    return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
 	}
 }
-=======
-   // Oracle 데이터베이스 연결 정보 설정
-      private static final String JDBC_URL = "jdbc:oracle:thin:@1.220.247.78:1522:orcl";
-      private static final String USER = "semi_project1";
-      private static final String PASSWORD = "123451";
-      
-      // 데이터베이스 연결을 반환하는 메서드
-      public static Connection getConnection() throws SQLException {
-          return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
-      }
-}
->>>>>>> 06ad83a3dae3af2cfde7d0a7b8865617aef86b03
